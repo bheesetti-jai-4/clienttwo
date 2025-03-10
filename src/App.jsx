e2d3bd2761c9708b38c116e7b8dc98b9e399fd47
bheesetti-jai-4 <jagannadhswamyjob>
2025-03-10 08:55:59 +0530

    adjust navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
-import React from 'react'
-import { BrowserRouter, Link, Route, Router, Routes  } from "react-router-dom";
+// import React from 'react'
+// import { BrowserRouter, Link, Route, Router, Routes  } from "react-router-dom";
+// import Home from './home/Home';
+// import NotFound from './NotFound';
+// import DoctorProfile from './profile/DoctorProfile';
+// import Services from './service/Services';
+// import GalaryCard from './galary/GalaryCard';
+// import Maps from './home/Maps';
+// import TestList from './testimonials/TestList';
+// import Navbar from './navbar/Navbar'
+
+
+// function App() {
+//   return (
+  
+//     <BrowserRouter>
+
+//     <Navbar/> 
+
+//     {/* links to display  */}
+//     {/* <Link to={"/home"} > home </Link> */}
+    
+
+//     <Routes>
+//     <Route  path={"*"} element={<NotFound/>} />
+//       <Route  path={"/"} element={<Home/>} />
+//       <Route  path={"/home"} element={<Home/>} />
+//       <Route  path={"/aboutme"} element={<DoctorProfile/>} />
+//       <Route  path={"/service"} element={<Services/>} />
+//       <Route  path={"/address"} element={<Maps/>} />
+//       {/* <Route  path={"/testmonial"} element={<Testimonial/>} /> */}
+//       <Route  path={"/testmonial"} element={<TestList/>} />
+//       <Route  path={"/galary"} element={<GalaryCard/>} />
+//     </Routes >
+      
+//       </BrowserRouter>
+  
+//   )
+// }
+
+// export default App
+
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './home/Home';
 import NotFound from './NotFound';
 import DoctorProfile from './profile/DoctorProfile';
@@ -7,37 +49,28 @@ import Services from './service/Services';
 import GalaryCard from './galary/GalaryCard';
 import Maps from './home/Maps';
 import TestList from './testimonials/TestList';
-// import Testimonial from './testimonials/Testimonials';
-// import TestList from './testimonials/TestList';
-// import Navbar from './navbar/Navbar'
-
+import Layout from './Layout'; // Import the Layout component
 
 function App() {
   return (
-  
     <BrowserRouter>
+      <Routes>
+        {/* Wrap pages that should have Navbar inside a Layout */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/aboutme" element={<DoctorProfile />} />
+          <Route path="/service" element={<Services />} />
+          <Route path="/address" element={<Maps />} />
+          <Route path="/testmonial" element={<TestList />} />
+          <Route path="/galary" element={<GalaryCard />} />
+        </Route>
 
-    {/* <Navbar/>  */}
-
-    {/* links to display  */}
-    {/* <Link to={"/home"} > home </Link> */}
-    
-
-    <Routes>
-    <Route  path={"*"} element={<NotFound/>} />
-      <Route  path={"/"} element={<Home/>} />
-      <Route  path={"/home"} element={<Home/>} />
-      <Route  path={"/aboutme"} element={<DoctorProfile/>} />
-      <Route  path={"/service"} element={<Services/>} />
-      <Route  path={"/address"} element={<Maps/>} />
-      {/* <Route  path={"/testmonial"} element={<Testimonial/>} /> */}
-      <Route  path={"/testmonial"} element={<TestList/>} />
-      <Route  path={"/galary"} element={<GalaryCard/>} />
-    </Routes >
-      
-      </BrowserRouter>
-  
-  )
+        {/* Not Found Page without Navbar */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+// import Navbar from './navbar/Navbar';
+
+function Layout() {
+  return (
+    <>
+      {/* <Navbar /> */}
+      <Outlet /> {/* This renders the nested routes */}
+    </>
+  );
+}
+
+export default Layout;
